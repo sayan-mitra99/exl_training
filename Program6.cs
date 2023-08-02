0f4275c5c677b30863a762067e552ae92f0226b0
sayan-mitra99 <100861305+sayan-mitra99@users.noreply.github.com>
2023-08-02 12:23:17 +0530

    Add files via upload

---
+++ Program6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Program6
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = File.ReadAllLines("d:\\youtube.csv");
+            Console.WriteLine(lines.Length-1);// total count
+            Console.WriteLine(lines[1].Length);
+            List<string> channel = new List<string>();
+            for (int i=0;i<lines.Length;i++)
+            {
+                channel.Add(lines[i].Split(",")[lines[i].Length-2]);
+            }
+            channel.Remove(channel[0]);
+            List<string> unique = channel.Distinct().ToList();
+            Console.WriteLine(unique.Count());// unique channel count
+            Console.WriteLine(channel.Count());
+            foreach(string i in unique)
+            {
+                Console.WriteLine(i);
+            }
+            // List<string> l = lines.ToList();
+            // Console.WriteLine(l.Count()-1);
+
+        }
+    }
+}
